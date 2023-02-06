ba3573e4e88092fa7c0a7e9189add31e53fb65aa
Daniel Marinescu <danielmarinescu2@gmail.com>
2023-02-06 07:34:19 -0500

    added API template code

--- job_board/etl/extract.py
+++ job_board/etl/extract.py
@@ -7,3 +7,17 @@ class Extract():
     '''
     ## code here
     pass
+
+
+url = "https://jsearch.p.rapidapi.com/search"
+
+querystring = {"query":"\"Data Analyst\" in USA ","num_pages":"20"}
+
+headers = {
+	"X-RapidAPI-Key": "5fec918ab1msh2089eaf59c35a4fp1d2106jsnc87acf303a5f",
+	"X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
