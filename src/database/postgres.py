c32b37b91d30307173648c3f2a57a44114c6d88b
Daniel Marinescu <danielmarinescu2@gmail.com>
2023-02-13 05:23:36 -0500

    updated postgres.py with dynamic port environment variable

--- src/database/postgres.py
+++ src/database/postgres.py
@@ -12,6 +12,7 @@ class PostgresDB():
         db_password = os.environ.get("db_password")
         db_server_name = os.environ.get("db_server_name")
         db_database_name = os.environ.get("db_database_name")
+        db_port = os.environ.get("db_port")
 
         # create connection to database 
         connection_url = URL.create(
@@ -19,7 +20,7 @@ class PostgresDB():
             username = db_user,
             password = db_password,
             host = db_server_name, 
-            port = 5432,
+            port = db_port,
             database = db_database_name, 
         )
 
